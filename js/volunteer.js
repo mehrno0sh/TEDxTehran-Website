c59570e2b041c2e78efb5dfd78939963018cee91
soheila <soheila.zangene@gmail.com>
2017-02-13 00:18:28 +0330

    volunteer revise

--- js/volunteer.js
+++ js/volunteer.js
@@ -1,19 +1,21 @@
 
-//var AvailablePosArray = ["curation|Curation Vice Executive","host|Host / Hostess", "media|Media Vice Executive",];
 var sel1 = "DepartmentsSelect";
 var sel2 = "Availablepositions";
 
+var avp = '{ "technology": ["iOS Application Developer", "Android Developer"],"curation": ["Curation Vice Executive"], "media":["Media Vice Executive"], "partnership":["Partnership Coordinator"], "operation" : ["Host/Hostess"]}';
+var availablePositions = JSON.parse(avp);
+
+
 $(document).ready(function(){
     var option = $('#DepartmentsSelect').children('option');
-    $(option[0]).prop('selected', true);
+    $(option["curation"]).prop('selected', true);
     filter_available_positions(sel1,sel2)
 })
 
 function apply_position(position) {
     var p = position;
-    console.log('click');
-    var option = $('#DepartmentsSelect').children('option');
-    $(option[p]).prop('selected', true);
+    
+    $('#DepartmentsSelect option[value=' + p + ']').prop('selected', true);
     
     filter_available_positions(sel1,sel2)
 }
@@ -22,30 +24,16 @@ function filter_available_positions(sel1, sel2){
     var sel1 = document.getElementById(sel1);
     var sel2 = document.getElementById(sel2);
     sel2.innerHTML = "";
-    var availablePositions;
-    if (sel1.value == 0) {
-       availablePositions = ["curation|Curation Vice Executive"];
-    }
-    else if (sel1.value == 1) {
-       availablePositions = ["media|Media Vice Executive"];
-    }
-    else if (sel1.value == 2) {
-       availablePositions = ["technology|iOS Application Developer"];
-    }
-    else if (sel1.value == 3) {
-       availablePositions = ["partnership|Partnership Coordinator"];
-    }
-    else if (sel1.value == 4) {
-       availablePositions = ["host|Host / Hostess"];
-    }
-
-
-    for (var option in availablePositions) {
-        var pair = availablePositions[option].split("|");
-        var newOption = document.createElement("option");
-        newOption.value = pair[0];
-        newOption.innerHTML = pair[1];
+   //var availablePositions;
+   var value = sel1.value;
+   roles = availablePositions[value];
+   
+   for (i=0; i<roles.length; i++) {
+         var newOption = document.createElement("option");
+        newOption.value = roles[i];
+        newOption.innerHTML = roles[i];
         sel2.options.add(newOption);
-    }
+   }
+    
     
 }
